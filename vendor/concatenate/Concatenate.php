763819542dfe0702d41ee76c2d13d5f020658e5c
Vinícius Ebersol <vebersol@gmail.com>
2013-03-11 10:34:26 -0300

    new version of concatenate

--- vendor/concatenate/Concatenate.php
+++ vendor/concatenate/Concatenate.php
@@ -3,12 +3,12 @@ Class Concatenate {
 	private $assetsPath =  '../../www/assets/';
 	private $dataPath = '../../src/';
 
-	public function __construct($type, $outputName) {
-		// $this->dataPath = $this->assetsPath . 'data/';
+	public function __construct($type, $outputName, $sourcePath = null, $outputPath = null, $jsonFile = null) {
 		$this->outputName = $outputName;
 		$this->type = $type;
-		$this->path = $this->assetsPath . $type . '/';
-		$this->sourcePath = $this->dataPath . $type . '/';
+		$this->path = $outputPath ? $outputPath : $this->assetsPath . $type . '/';
+		$this->sourcePath = $sourcePath ? $sourcePath : $this->dataPath . $type . '/';
+		$this->jsonFile = $jsonFile ? $jsonFile : 'files_manifest.json';
 
 		$this->jsonData = $this->_loadJson();
 		$this->files = $this->jsonData->$type;
@@ -19,17 +19,17 @@ Class Concatenate {
 	private function _call() {
 		$result = '';
 		$result .= '/* '. date("Y/m/d H:i:s") . "*/\n\n";
-		
+
 		for ($i = 0; $i < count($this->files); $i++) {
 			$partialContent = '';
 			$partialContent .= '/* ' . $this->files[$i] . '*/';
 			$partialContent .= "\n";
 			$partialContent .= file_get_contents($this->sourcePath . $this->files[$i]);
 			$partialContent .= "\n\n";
-			
+
 			$result .= $partialContent;
 		}
-		
+
 		echo $result;
 
 		// if is localhost update js file
@@ -39,18 +39,18 @@ Class Concatenate {
 	}
 
 	private function _loadJson() {
-		$json = file_get_contents($this->dataPath . 'files_manifest.json');
+		$json = file_get_contents($this->dataPath . $this->jsonFile);
 
 		return json_decode($json);
 	}
 
 	private function _writeToFile($result) {
 		$file = $this->path . $this->outputName . '.' . $this->type;
-		
+
 		if (file_exists($file)) {
 			unlink($file);
 		}
-		
+
 		file_put_contents($file, $result);
 	}
 }
