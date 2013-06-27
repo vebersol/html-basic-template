476d94d5e0a2ced69aa387b2e36fe015cafa2ee9
Vinícius Ebersol <vebersol@gmail.com>
2013-06-27 17:34:28 -0300

    remove workaround that ignores icons generated hash in filename

--- src/config.rb
+++ src/config.rb
@@ -36,19 +36,19 @@ http_generated_images_path = "../img" # the saved sprites path (from output css 
 asset_cache_buster :none
 
 # Make a copy of sprites with a name that has no uniqueness of the hash.
-on_sprite_saved do |filename|
-  if File.exists?(filename)
-    FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
-  end
-end
+# on_sprite_saved do |filename|
+#   if File.exists?(filename)
+#     FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+#   end
+# end
 
 # # Replace in stylesheets generated references to sprites
 # # by their counterparts without the hash uniqueness.
-on_stylesheet_saved do |filename|
-  if File.exists?(filename)
-    css = File.read filename
-    File.open(filename, 'w+') do |f|
-      f << css.gsub(%r{-s([a-z0-9]{10})\.png}, '.png?v\1')
-    end
-  end
-end
+# on_stylesheet_saved do |filename|
+#   if File.exists?(filename)
+#     css = File.read filename
+#     File.open(filename, 'w+') do |f|
+#       f << css.gsub(%r{-s([a-z0-9]{10})\.png}, '.png?v\1')
+#     end
+#   end
+# end
