8531d70f3502baf71f32e4b67183cb0d4fe87e74
Vinícius Ebersol <vebersol@gmail.com>
2013-06-27 17:13:37 -0300

    compass config file

---
+++ src/config.rb
@@ -0,0 +1,54 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../www/assets/css"
+sass_dir = "sass"
+images_dir = "../www/assets/img/"
+javascripts_dir = "js/lib/compass" # where compass libs will be available
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+http_images_path = "../img" # the images path (from output css folder)
+#generated_images_dir = "../www/assets/img/" # saved sprites path
+http_generated_images_path = "../img" # the saved sprites path (from output css folder)
+
+# default behavior of compass sprite generator generates images with icons-#{hash}.png
+# In order to avoid lots of image files in your repository, it changes how sprite generator works
+# to icons.png and add a randomic number after the image name, something like: icons.png?12345
+
+asset_cache_buster :none
+
+# Make a copy of sprites with a name that has no uniqueness of the hash.
+on_sprite_saved do |filename|
+  if File.exists?(filename)
+    FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+  end
+end
+
+# # Replace in stylesheets generated references to sprites
+# # by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s([a-z0-9]{10})\.png}, '.png?v\1')
+    end
+  end
+end
